967b4fc2e78522c463c14b394fcb5d6fe1e3e669
john <john@johnzeren.com>
2012-12-30 23:31:35 -0500

    Added TileCreator class to help with refactoring of Initializer.createTiles later... not functioning yet because we have to keep track of count to break out of the loop when we hit a certian number.

---
+++ src/com/majong/engine/TileCreator.java
@@ -0,0 +1,127 @@
+package com.majong.engine;
+
+import java.util.ArrayList;
+
+import com.majong.structure.tiles.Bamboos;
+import com.majong.structure.tiles.Characters;
+import com.majong.structure.tiles.Dots;
+import com.majong.structure.tiles.Flowers;
+import com.majong.structure.tiles.Honors;
+import com.majong.structure.tiles.Seasons;
+import com.majong.structure.tiles.Tile;
+import com.majong.structure.tiles.TileType;
+
+public class TileCreator {
+
+	public ArrayList<Tile> createBamboos(ArrayList<Tile> tiles, TileType type){
+		System.out.println("TYpe is" + TileType.BAMBOOS);
+		//make bamboos
+		for(Bamboos bamboo : Bamboos.values()){
+			Tile tile = new Tile();
+			tile.setGraphic(bamboo.toString());
+			tile.setType(type);
+			tiles.add(tile);
+			tile.setGraphic(bamboo.toString());
+			tile.setType(type);
+			tiles.add(tile);
+			
+			System.out.println("TYpe: " + bamboo);		
+		}
+		
+		return tiles;
+	}
+	
+	public ArrayList<Tile> createCharacters(ArrayList<Tile> tiles, TileType type){
+		System.out.println("TYpe is Chars" + TileType.CHARACTERS);
+		//make characters
+		for(Characters characters : Characters.values()){
+			Tile tile = new Tile();
+			tile.setGraphic(characters.toString());
+			tile.setType(type);
+			tiles.add(tile);
+			tile.setGraphic(characters.toString());
+			tile.setType(type);
+			tiles.add(tile);
+			
+			System.out.println("TYpe: " + characters);
+
+		}
+		
+		return tiles;
+	}
+	
+	public ArrayList<Tile> createDots(ArrayList<Tile> tiles, TileType type){
+		System.out.println("TYpe is " + TileType.DOTS);
+		//make dots
+		for(Dots dots : Dots.values()){
+			Tile tile = new Tile();
+			tile.setGraphic(dots.toString());
+			tile.setType(type);
+			tiles.add(tile);
+			tile.setGraphic(dots.toString());
+			tile.setType(type);
+			tiles.add(tile);
+			
+			System.out.println("TYpe: " + dots);
+		}
+		
+		return tiles;
+	}
+	
+	public ArrayList<Tile> createHonors(ArrayList<Tile> tiles, TileType type){
+		System.out.println("TYpe is " + TileType.HONORS);
+		//make honors
+		for(Honors honors : Honors.values()){
+			Tile tile = new Tile();
+			tile.setGraphic(honors.toString());
+			tile.setType(type);
+			tiles.add(tile);
+			tile.setGraphic(honors.toString());
+			tile.setType(type);
+			tiles.add(tile);
+			
+			System.out.println("TYpe: " + honors);
+		}
+		
+		return tiles;
+	}
+	
+	public ArrayList<Tile> createFlowers(ArrayList<Tile> tiles, TileType type){
+		System.out.println("TYpe is " + TileType.FLOWERS);
+		//make flowers
+		for(Flowers flowers : Flowers.values()){
+			Tile tile = new Tile();
+			tile.setGraphic(flowers.toString());
+			tile.setType(type);
+			tiles.add(tile);
+			tile.setGraphic(flowers.toString());
+			tile.setType(type);
+			tiles.add(tile);
+			
+			System.out.println("TYpe: " + flowers);
+
+		}
+		
+		return tiles;
+	}
+	
+	public ArrayList<Tile> createSeasons(ArrayList<Tile> tiles, TileType type){
+		System.out.println("TYpe is " + TileType.SEASONS);
+		//make seasons
+		for(Seasons seasons : Seasons.values()){
+			Tile tile = new Tile();
+			tile.setGraphic(seasons.toString());
+			tile.setType(type);
+			tiles.add(tile);
+			tile.setGraphic(seasons.toString());
+			tile.setType(type);
+			tiles.add(tile);
+			
+			System.out.println("TYpe: " + seasons);
+			
+		}		
+		
+		return tiles;
+	}
+	
+}
